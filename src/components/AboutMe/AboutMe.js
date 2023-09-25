c7dbb61b8b4d5b8de63b6569efd6f246a6157979
Elena Milosevic <elenagiura94@gmail.com>
2023-09-25 14:56:25 +0200

    updating node version and fixing problems caused by that

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,50 +6,57 @@ import cvImage from '../../images/CV-photo.jpg';
 import cv from '../../elena-milosevic.pdf';
 
 const AboutMe = () => {
+  React.useEffect(() => {
+    Aos.init({ duration: 3000, once: true });
+  }, []);
 
-	React.useEffect(()=>{
-		Aos.init({duration: 3000, once:true});
-	}, []);
+  const openCV = (e) => {
+    e.preventDefault();
+    window.open(cv);
+  };
 
-	const openCV = (e) => {
-		e.preventDefault();
-		window.open(cv);
-	}
-
-	return (
-		<section className='about-me'>
-			<div className='wrapper'> 
-				<h2 data-aos='zoom-in-up'>ABOUT ME</h2>
-				<img src={cvImage} alt='Elena Milosevic' data-aos='zoom-in-left'/>
-				<section data-aos='zoom-in-right'>
-					<p>Hello world! My name is Elena Milosevic and I enjoy creating things that live on the internet. <br/>
-					I graduated on the Faculty of Mechanical Engineering at the University of Belgrade,
-					and achived the title of M.Sc. in Mechanical Engineering. 
-					I started learning web design on my own out of curiosity and as I really liked it,
-					I decided to change the direction of my career towards front-end development,
-					as it suits me better and better expresses my creativity. <br/>
-					My Moto is quote from Mahatma Gandhi who briefly explained all that is needed for happiness:  &nbsp;
-					<q>Be the change that you wish to see in the world.</q>
-					<br/><br/>
-					</p>
-					<p>My current stack of languages/technologies are:</p>
-					<ul>
-						<li>React.Js</li>
-						<li>JavaScript</li>
-						<li>TypeScript</li>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>SASS</li>
-						<li>Git</li>
-						<li>GitHub</li>
-						<li>NPM</li>
-						<li>VS Code</li>
-					</ul>
-					<a onClick={(e)=>openCV(e)} target='_blank' id='cv' href='#'><span>Curriculum Vitae</span><span></span></a>
-				</section>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section className='about-me'>
+      <div className='wrapper'>
+        <h2 data-aos='zoom-in-up'>ABOUT ME</h2>
+        <img src={cvImage} alt='Elena Milosevic' data-aos='zoom-in-left' />
+        <section data-aos='zoom-in-right'>
+          <p>
+            Hello world! My name is Elena Milosevic and I enjoy creating things
+            that live on the internet. <br />
+            I graduated on the Faculty of Mechanical Engineering at the
+            University of Belgrade, and achived the title of M.Sc. in Mechanical
+            Engineering. I started learning web design on my own out of
+            curiosity and as I really liked it, I decided to change the
+            direction of my career towards front-end development, as it suits me
+            better and better expresses my creativity. <br />
+            My Moto is quote from Mahatma Gandhi who briefly explained all that
+            is needed for happiness: &nbsp;
+            <q>Be the change that you wish to see in the world.</q>
+            <br />
+            <br />
+          </p>
+          <p>My current stack of languages/technologies are:</p>
+          <ul>
+            <li>React.Js</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>NPM</li>
+            <li>VS Code</li>
+          </ul>
+          <button onClick={(e) => openCV(e)} target='blank' id='cv'>
+            <span>Curriculum Vitae</span>
+            <span></span>
+          </button>
+        </section>
+      </div>
+    </section>
+  );
+};
 
 export default AboutMe;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,33 @@ import linkedIn from '../../images/linkedin.png';
 import arrowUp from '../../images/arrow-up.png';
 
 const Footer = () => {
+  const goUp = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
 
-	const goUp = () => {
-		window.scrollTo({
-		  top: 0,
-		  left: 0,
-		  behavior: 'smooth'
-		});
-	}
-
-	return (
-		<footer>
-			<div className='wrapper'> 
-				<a onClick={goUp}><img src={arrowUp} alt='Arrow up'/></a>
-				<div className='social-networks'>
-					<a href='https://github.com/elenagiura' target='blank'><img src={gitHub} alt='GitHub'/></a>
-					<a href='https://www.linkedin.com/in/elena-giura/' target='blank'><img src={linkedIn} alt='LinkedIn'/></a>
-				</div>
-				<hr/>
-				<p>&#169; 2021 - Template developed by Elena Milosevic</p>
-			</div>
-		</footer>
-	)
-}
+  return (
+    <footer>
+      <div className='wrapper'>
+        <button onClick={goUp}>
+          <img src={arrowUp} alt='Arrow up' />
+        </button>
+        <div className='social-networks'>
+          <a href='https://github.com/elenagiura' target='blank'>
+            <img src={gitHub} alt='GitHub' />
+          </a>
+          <a href='https://www.linkedin.com/in/elena-giura/' target='blank'>
+            <img src={linkedIn} alt='LinkedIn' />
+          </a>
+        </div>
+        <hr />
+        <p>&#169; 2021 - Template developed by Elena Milosevic</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
